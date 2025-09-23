fea5c6e064326171d86e3ae3e50c78baf03a062e
questral <kai.okorodudu@gmail.com>
2025-09-22 23:03:28 -0400

    ported old website to svelte

---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,22 @@
+<!-- <nav>
+    <ul>
+        <li><a href="../index.html">home</a></li>
+        <li><a href="../projects/index.html">projects</a></li>
+        <li><a href="#">contacts</a></li>
+        <li><a href="../resume/index.html">resume</a></li>
+        <li><a href="https://github.com/questral" target="_blank">my git</a></li>
+    </ul>
+</nav> -->
+
+<svelte:head>
+	<title>contacts</title>
+</svelte:head>
+
+<h1>contacts</h1>
+
+<form action="mailto:foo@example.com" method="get">
+    <!-- <label>email<input name="email" type="email" value="h@email.com"></label> -->
+    <label>subject<input name="subject" value="hello"></label>
+    <label>body<textarea name="body">hello from this box</textarea></label>
+    <button>submit</button>
+</form>
